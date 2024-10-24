2fc202c216ccfb6585432beed5619181ccb61c1d
pablo-r-d <pruidom319@g.educaand.es>
2024-10-24 19:58:48 +0200

    Creo Header.css como hoja de estilos provisional para el header

---
+++ frontend/VhyperGamesClient/src/components/Header.jsx
@@ -0,0 +1,14 @@
+// import './Button.jsx'
+import './Header.css';
+
+function Header() {
+
+    return (
+        <>
+            <div class="Header">
+                <img class="logo" src="logo" alt="Logo" />
+            </div>
+        </>
+    );
+
+}
