f8c6fccc3bce3b62bd2b5003a64aeddf968666f1
Jmolmel <josemolinamelendez@gmail.com>
2024-10-24 10:17:58 +0200

    Se crea MyDbContext y clase User

---
+++ backend/VhyperGamesServer/VhyperGamesServer/Database/MyDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VhyperGamesServer.Database;
+
+public class MyDbContext : DbContext
+{
+    private const string DATABASE_PATH = "vhypergames.db";
+
+    //Tablas
+    public DbSet<User> Users { get; set; }
+
+
+    //Configurar el proveedor de base de datos Sqlite
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        //AppDomain obtiene el directorio donde se ejecuta la aplicación
+        string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
+        // Se configura Sqlite como proveedor de BD pasando la ruta de archivo ("vhypergames.db) en el directorio base de la aplicacion
+        options.UseSqlite($"DataSource={baseDir}{DATABASE_PATH}");
+    }
+
+}
---
+++ backend/VhyperGamesServer/VhyperGamesServer/Database/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VhyperGamesServer.Database;
+
+public class User
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Email { get; set; }
+
+    [Required]
+    [MaxLength(40)]
+    public string Password { get; set; }
+
+    [Required]
+    public string Rol {  get; set; }
+
+    [Required]
+    [MaxLength(250)]
+    public string Address { get; set; }
+}
