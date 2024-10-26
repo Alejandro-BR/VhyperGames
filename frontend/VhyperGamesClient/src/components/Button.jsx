c599a6d72cbb291eb69369f8899766a26e637ef5
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-10-26 17:28:33 +0200

    Button

---
+++ frontend/VhyperGamesClient/src/components/Button.jsx
@@ -0,0 +1,30 @@
+import styles from './Button.module.css';
+
+/**
+ * Button Component
+ * 
+ * Este componente renderiza un botón personalizado, permitiendo diferentes variantes.
+ *
+ * @param {Object} props - Propiedades para configurar el componente.
+ * @param {React.ReactNode} props.children - Contenido.
+ * @param {Function} props.onClick - Función que se ejecuta.
+ * @param {string} props.variant - Tamaño (`short`, `large`, `medium`).
+ * @param {string} props.color - Color (`azul`, `morado`, `azul-morado`, `morado-azul`).
+ * 
+ * @returns {JSX.Element} Componente `<Button />`.
+ * 
+ * @example
+ * <Button variant="short" color="azul" onClick={function}>Texto</Button>
+ */
+function Button({ children, onClick, variant, color}) {
+  return (
+    <button 
+      className={`${styles[variant]} ${styles[color]}` }
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
--- frontend/VhyperGamesClient/src/main.jsx
+++ frontend/VhyperGamesClient/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './global.css'
 import App from './App.jsx'
+import DocBotones from './components/placeHolders/DocBotones.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App/>
+    <DocBotones/>
   </StrictMode>,
 )
