88b114a0b0566ccc2a3b00fee14f19b14e3e365e
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-10-26 19:13:34 +0200

    Cambios en User

--- backend/VhyperGamesServer/VhyperGamesServer/Models/Database/Entities/User.cs
+++ backend/VhyperGamesServer/VhyperGamesServer/Models/Database/Entities/User.cs
@@ -1,31 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace VhyperGamesServer.Models.Database.Entities;
+﻿namespace VhyperGamesServer.Models.Database.Entities;
 
 public class User
 {
     public int Id { get; set; }
 
-    [Required]
-    [MaxLength(100)]
     public string Name { get; set; }
 
-    [Required]
-    [MaxLength(100)]
     public string Surname { get; set; }
 
-    [Required]
-    [MaxLength(100)]
     public string Email { get; set; }
 
-    [Required]
-    [MaxLength(40)]
-    public string Password { get; set; }
+    public string HashPassword { get; set; }
 
-    [Required]
     public string Rol { get; set; }
 
-    [Required]
-    [MaxLength(250)]
     public string Address { get; set; }
 }
