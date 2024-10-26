eb65d59dc40cfa917e23a0a90bdbebcd2be83112
Raquel <rlopber070@g.educaand.es>
2024-10-26 11:47:44 +0200

    Creando el AuthController

---
+++ backend/VhyperGamesServer/VhyperGamesServer/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using VhyperGamesServer.Models.Database.Repositories;
+using VhyperGamesServer.Models.Dtos;
+
+namespace VhyperGamesServer.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly TokenValidationParameters _tokenParameters;
+    private readonly UnitOfWork _unitOfWork;
+
+    public AuthController(IOptionsMonitor<JwtBearerOptions> jwtOptions)
+    {
+        _tokenParameters = jwtOptions.Get(JwtBearerDefaults.AuthenticationScheme)
+            .TokenValidationParameters;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<AccessTokenJws>> Login([FromBody] LoginRequest request)
+    {
+
+        var user = await _unitOfWork.UserRepository.UserValidate(request.Email.ToLower(), request.Password);
+        if (request.Email == )
+    }
+}
