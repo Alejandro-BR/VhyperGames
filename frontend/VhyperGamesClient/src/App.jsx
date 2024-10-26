2130673b2dad2c2434a0bbac277ffe3bcf246817
fernandosanchez1609 <fsanlag686@g.educaand.es>
2024-10-26 02:58:44 +0200

    muchas cosas frontend

--- frontend/VhyperGamesClient/src/App.jsx
+++ frontend/VhyperGamesClient/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import Footer from './components/footer';
+import Body from './components/placeHolders/bodyPlaceHolder'
 function App() {
 
 
   return (
       <>
       <Header />
+      <Body/>
       <Footer />
       </>
   )
---
+++ frontend/VhyperGamesClient/src/components/placeHolders/bodyPlaceHolder.jsx
@@ -0,0 +1,34 @@
+function Body() {
+  return(
+    <body>
+    <h1>Inicio de la Página</h1>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <p>Contenido de prueba...</p>
+    <h1>Fin de la Página</h1>
+  </body>
+  )
+}
+
+export default Body;
