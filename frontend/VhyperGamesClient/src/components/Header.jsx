72f57063bc7a6449c83046be983598348460c3c4
pablorzdmngz <pablorzdmngz@gmail.com>
2024-10-27 22:03:50 +0100

    Pruebas fallidas carousel

--- frontend/VhyperGamesClient/src/components/Header.jsx
+++ frontend/VhyperGamesClient/src/components/Header.jsx
@@ -1,6 +1,6 @@
 // import './Button.jsx'
 import classes from './Header.module.css';
-import classes from './Header.module.css';
+// import classes from './Header.module.css';
 import Button from './Button';
 // import Button from './placeHolders/buttonPlaceHolder';
 
