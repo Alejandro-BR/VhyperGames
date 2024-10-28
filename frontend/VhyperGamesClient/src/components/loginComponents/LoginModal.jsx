61ca5b4005b995f64183c2e375de3e724d5427c7
Jmolmel <josemolinamelendez@gmail.com>
2024-10-29 00:31:48 +0100

    Implemento fetchingData, problemas con decodedToken

--- frontend/VhyperGamesClient/src/components/loginComponents/LoginModal.jsx
+++ frontend/VhyperGamesClient/src/components/loginComponents/LoginModal.jsx
@@ -2,54 +2,80 @@ import { useRef, useState } from 'react';
 import styles from './Login.module.css';
 import Button from '../buttonComponent/Button';
 import RegisterModal from '../registerComponents/RegisterModal';
+import * as jwt_decode from 'jwt-decode';
+
 
 function LoginModal({ onClose }) {
-    const emailRef = useRef();
-    const passwordRef = useRef();
-    const [errorMessage, setErrorMessage] = useState('');
-    const [mostrarRegister, setMostrarRegister] = useState(false)
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    const [promesaError, setPromesaError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [mostrarRegister, setMostrarRegister] = useState(false);
 
     const handleRegister = () => {
         setMostrarRegister(true);
     };
 
-
     const handleSubmit = async (event) => {
         event.preventDefault();
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
 
+        const emailValue = emailRef.current.value;
+        const passwordValue = passwordRef.current.value;
 
+        const objetoBackend = {
+            email: emailValue,
+            password: passwordValue,
+        };
+
+        await fetchingData('https://localhost:7207/api/auth/login', objetoBackend);
+    };
+
+    async function fetchingData(url, data) {
         try {
-            const response = await fetch('https://localhost:7207/api/auth/login', {
+            setIsLoading(true);
+            const response = await fetch(url, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ email, password }),
+                body: JSON.stringify(data),
             });
 
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.message || "Error al iniciar sesión.");
+            if (response.ok) {
+                const datosPromesa = await response.json();
+                const token = datosPromesa.accessToken; // Ajusta esto según el formato de tu respuesta
+                console.log('Token recibido:', token)
+
+
+                // const decodedToken = jwt_decode(token);
+
+                // if (decodedToken) {
+                //     const userInfo = {
+                //         id: decodedToken.id,  // Asegúrate de que 'id' existe
+                //         role: decodedToken.role,  // Asegúrate de que 'role' existe
+                //     };
+                //     console.log("userInfo", userInfo);
+                // }
+
+                // console.log('Inicio de sesión exitoso:', decodedToken);
+
+                onClose(); // Cierra el modal al iniciar sesión exitosamente
+            } else {
+                setPromesaError("Error en el servidor");
             }
-
-            const data = await response.json();
-            const { accessToken } = data;
-            console.log('Inicio de sesión exitoso:', accessToken);
-            localStorage.setItem('accessToken', accessToken);
-            onClose(); // Cierra el modal al iniciar sesión exitosamente
-
         } catch (error) {
-            setErrorMessage(error.message);
-            console.error('Error en el login:', error);
+            console.log(error);
+            setPromesaError(`Error en el servidor: ${error.message}`);
+        } finally {
+            setIsLoading(false);
         }
-    };
+    }
+
 
     return (
         <div className={styles.modalOverlay}>
             <div className={styles.loginModule}>
-            <button className={styles.logoCerrar} onClick={onClose}>
+                <button className={styles.logoCerrar} onClick={onClose}>
                     <img src="./public/icon/cerrar-icon.svg" alt="icono cerrar" />
                 </button>
 
@@ -58,7 +84,7 @@ function LoginModal({ onClose }) {
                 </div>
 
                 <form className={styles.formContainer} onSubmit={handleSubmit}>
-                    {errorMessage && <div className={styles.error}>{errorMessage}</div>}
+                    
 
                     <div className={styles.inputGroup}>
                         <input
@@ -81,16 +107,17 @@ function LoginModal({ onClose }) {
                             required
                         />
                     </div>
-
+                    
                     <div className={styles.rememberMe}>
                         <input type="checkbox" id="rememberMe" />
                         <label htmlFor="rememberMe">Recuérdame</label>
                     </div>
 
+                    {promesaError && <div className={styles.error}>{promesaError}</div>}
 
-                    <div className={styles.buttonContainer}> 
-                        <Button type="submit" variant='large' color='morado'>
-                            Iniciar Sesión
+                    <div className={styles.buttonContainer}>
+                        <Button type="submit" variant='large' color='morado' disabled={isLoading}>
+                            {isLoading ? "Cargando..." : "Iniciar Sesión"}
                         </Button>
 
                         <Button variant='large' color='azul' onClick={handleRegister}>
