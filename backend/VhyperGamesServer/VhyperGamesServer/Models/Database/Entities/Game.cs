aa1560eae84d3f1534502636301db48373d93508
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-01 13:51:59 +0100

    Entities Game y ImageGame

---
+++ backend/VhyperGamesServer/VhyperGamesServer/Models/Database/Entities/Game.cs
@@ -0,0 +1,24 @@
+﻿namespace VhyperGamesServer.Models.Database.Entities;
+
+public class Game
+{
+    public int Id { get; set; }
+    public Guid GameCode {  get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Genre { get; set; }
+    public bool DrmFree {  get; set; }
+    public DateTime ReleaseDate {  get; set; }
+    public decimal Price { get; set; }
+    public int Stock {  get; set; }
+    public int AvgRating { get; set; }
+    public List<ImageGame> ImageGames { get; set; }
+
+    public Game()
+    {
+        GameCode = Guid.NewGuid();
+        DrmFree = false;
+        //ImageGames = new List<ImageGame>();
+        ImageGames = [];
+    }
+}
