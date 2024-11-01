0f1c1957e5fd87353df73e7f28afacc68fd405c3
Raquel <rlopber070@g.educaand.es>
2024-11-01 21:38:35 +0100

    Creado GameFilterDto

---
+++ backend/VhyperGamesServer/VhyperGamesServer/Models/Dtos/GameFilterDto.cs
@@ -0,0 +1,21 @@
+﻿namespace VhyperGamesServer.Models.Dtos;
+
+public class GameFilterDto
+{
+    public string SearchText { get; set; }
+    public string SortCriteria { get; set; }
+    public bool? DrmFree { get; set; }
+    public string Genre { get; set; }
+    public int ResultsPerPage { get; set; }
+    public int Page { get; set; }
+
+    public GameFilterDto()
+    {
+        SearchText = null;
+        SortCriteria = null;
+        DrmFree = null;
+        Genre = null;
+        ResultsPerPage = 10;
+        Page = 1;
+    }
+}
