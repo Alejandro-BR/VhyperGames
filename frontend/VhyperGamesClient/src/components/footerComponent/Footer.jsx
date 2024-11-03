16ecfb7f193b81b51888b7e7021b676b5ac0a028
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-03 16:20:36 +0100

    Navegacion al home desde footer

--- frontend/VhyperGamesClient/src/components/footerComponent/Footer.jsx
+++ frontend/VhyperGamesClient/src/components/footerComponent/Footer.jsx
@@ -2,17 +2,20 @@
 import classes from './FooterExperimental.module.css'
 import Button from '../buttonComponent/Button';
 import { messageAboutUs, messageCompany } from '../../helpers/messages';
+import { NavLink } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className={classes.footer}>
       <div className={classes.leftFooter}>
-        <img
-          className={classes.imgFooter}
-          src="/img/LogoVG.png"
-          alt="Logo"
-          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-        />
+        <NavLink
+          to="/">
+          <img
+            className={classes.imgFooter}
+            src="/img/LogoVG.png"
+            alt="Logo"
+          />
+        </NavLink>
         <div className={classes.text}>
           <h1>VHYPER GAMES</h1>
           <p>© 2024 | VHYPER GAMES | All rights reserved</p>
@@ -21,41 +24,41 @@ function Footer() {
 
       <div className={classes.rightFooter}>
 
-          <div className={classes.fila}>
-            <img
-              className={classes.icon}
-              src="/img/X.PNG"
-              alt="X"
-              onClick={() => window.open("https://x.com/", "_blank")}
-            />
-            <Button variant="large" color="morado" onClick={messageAboutUs}>
-              Sobre nosotros
+        <div className={classes.fila}>
+          <img
+            className={classes.icon}
+            src="/img/X.PNG"
+            alt="X"
+            onClick={() => window.open("https://x.com/", "_blank")}
+          />
+          <Button variant="large" color="morado" onClick={messageAboutUs}>
+            Sobre nosotros
+          </Button>
+        </div>
+        <div className={classes.fila}>
+          <img
+            className={classes.icon}
+            src="/img/Instagram.PNG"
+            alt="Instagram"
+            onClick={() => window.open("https://www.instagram.com/", "_blank")}
+          />
+          <Button variant="large" color="morado" onClick={messageCompany}>
+            Empresa
+          </Button>
+        </div>
+        <div className={classes.fila}>
+          <img
+            className={classes.icon}
+            src="/img/linkedin.PNG"
+            alt="LinkedIn"
+            onClick={() => window.open("https://es.linkedin.com/", "_blank")}
+          />
+          <a href="https://github.com/VhyperGames" target="_blank" rel="noopener noreferrer">
+            <Button variant="large" color="azul">
+              <span className={classes.logoGit}>< img src="/icon/cib_github.svg" alt="GitHub" /></span>GitHub
             </Button>
-          </div>
-          <div className={classes.fila}>
-            <img
-              className={classes.icon}
-              src="/img/Instagram.PNG"
-              alt="Instagram"
-              onClick={() => window.open("https://www.instagram.com/", "_blank")}
-            />
-            <Button variant="large" color="morado" onClick={messageCompany}>
-              Empresa
-            </Button>
-          </div>
-          <div className={classes.fila}>
-            <img
-              className={classes.icon}
-              src="/img/linkedin.PNG"
-              alt="LinkedIn"
-              onClick={() => window.open("https://es.linkedin.com/", "_blank")}
-            />
-            <a href="https://github.com/VhyperGames" target="_blank" rel="noopener noreferrer">
-              <Button variant="large" color="azul">
-                <span className={classes.logoGit}>< img src="/icon/cib_github.svg" alt="GitHub" /></span>GitHub
-              </Button>
-            </a>
-          </div>
+          </a>
+        </div>
 
       </div>
     </footer>
