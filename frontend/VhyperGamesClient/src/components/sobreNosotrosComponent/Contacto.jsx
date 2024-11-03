e72f76b00d3f572b656924572772a35986fa733a
Jmolmel <josemolinamelendez@gmail.com>
2024-11-03 23:56:11 +0100

    GoogleMaps en SobreNosotros

--- frontend/VhyperGamesClient/src/components/sobreNosotrosComponent/Contacto.jsx
+++ frontend/VhyperGamesClient/src/components/sobreNosotrosComponent/Contacto.jsx
@@ -27,12 +27,12 @@ function Contacto() {
           <h3>¿Quiénes somos?</h3>
         </div>
         <div className={styles.location}>
-          <iframe
-            src={`https://www.google.com/maps/embed/v1/view?key=TU_CLAVE_API&center=36.74059156245471,-4.554462802812171&zoom=14`}
-            width="600"
-            height="450"
-            style={{ border: 0 }}
-            allowFullScreen=""
+          <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509234!2d144.95373631511655!3d-37.81627957975199!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f0bafc1%3A0x5045675218ceed0!2sC%2F%20Frederick%20Terman%2C%203%20%7C%20Google!5e0!3m2!1sen!2sus!4v1614027070742!5m2!1sen!2sus" 
+            width="600" 
+            height="450" 
+            style={{ border: 0 }} 
+            allowFullScreen="" 
             loading="lazy">
           </iframe>
         </div>
