23fd811ca647bd73713f928747031c3f6bf6effa
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-03 13:58:20 +0100

    react-router-dom

--- frontend/VhyperGamesClient/src/App.jsx
+++ frontend/VhyperGamesClient/src/App.jsx
@@ -1,9 +1,26 @@
+import RootLayout from './pages/RootLayout';
 import Home from './pages/Home';
+import Error from './pages/Error';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+
+const routeDefinition = createRoutesFromElements(
+  <Route path="/" element={<RootLayout/>} errorElement={<Error />}>
+    <Route index element={<Home />} />
+    <Route path="error" element={<Error />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routeDefinition);
 
 function App() {
-  return (
-      <Home />
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
