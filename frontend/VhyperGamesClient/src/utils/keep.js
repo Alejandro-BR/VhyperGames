c9fcb047e659f865cffa72d0501b9aacaeddeb2a
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-03 19:42:13 +0100

    keep mejorado

--- frontend/VhyperGamesClient/src/utils/keep.js
+++ frontend/VhyperGamesClient/src/utils/keep.js
@@ -1,23 +1,57 @@
-export function updateLocalStorage(item, modo) {
-  localStorage.setItem(modo, JSON.stringify(item));
+/**
+ * Actualiza el almacenamiento local con un nuevo ítem.
+ * 
+ * @param {object[]} item - El objeto o array que se va a almacenar en localStorage.
+ * @param {string} clave - La clave bajo la cual se guardará el ítem en localStorage.
+ */
+export function updateLocalStorage(item, clave) {
+  localStorage.setItem(clave, JSON.stringify(item));
 }
 
-export function updateSessionStorage(item, modo) {
-  sessionStorage.setItem(modo, JSON.stringify(item));
+/**
+ * Actualiza el almacenamiento de sesión con un nuevo ítem.
+ * 
+ * @param {object[]} item - El objeto o array que se va a almacenar en sessionStorage.
+ * @param {string} clave - La clave bajo la cual se guardará el ítem en sessionStorage.
+ */
+export function updateSessionStorage(item, clave) {
+  sessionStorage.setItem(clave, JSON.stringify(item));
 }
 
-export function getVarLS(modo) {
-  return JSON.parse(localStorage.getItem(modo)) || [];
+/**
+ * Obtiene una variable del almacenamiento local.
+ * 
+ * @param {string} clave - La clave bajo la cual se guardó el ítem en localStorage.
+ * @returns {object[]} - El objeto o array recuperado del almacenamiento local, o un array vacío.
+ */
+export function getVarLS(clave) {
+  return JSON.parse(localStorage.getItem(clave)) || [];
 }
 
-export function getVarSS(modo) {
-  return JSON.parse(sessionStorage.getItem(modo)) || [];
+/**
+ * Obtiene una variable del almacenamiento de sesión.
+ * 
+ * @param {string} clave - La clave bajo la cual se guardó el ítem en sessionStorage.
+ * @returns {object[]} - El objeto o array recuperado del almacenamiento de sesión, o un array vacío.
+ */
+export function getVarSS(clave) {
+  return JSON.parse(sessionStorage.getItem(clave)) || [];
 }
 
-export function deleteLocalStorage(modo) {
-  localStorage.removeItem(modo); 
+/**
+ * Elimina un ítem del almacenamiento local.
+ * 
+ * @param {string} clave - La clave del ítem que se va a eliminar del localStorage.
+ */
+export function deleteLocalStorage(clave) {
+  localStorage.removeItem(clave);
 }
 
-export function deleteSessionStorage(modo) {
-  sessionStorage.removeItem(modo);
+/**
+ * Elimina un ítem del almacenamiento de sesión.
+ * 
+ * @param {string} clave - La clave del ítem que se va a eliminar del sessionStorage.
+ */
+export function deleteSessionStorage(clave) {
+  sessionStorage.removeItem(clave);
 }
