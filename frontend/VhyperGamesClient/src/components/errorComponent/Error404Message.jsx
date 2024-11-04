abb5c7d9dc09fbee28b20841c77cf7f34866131c
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-04 20:11:12 +0100

    Solucion de errores en la pagina error 404

--- frontend/VhyperGamesClient/src/components/errorComponent/Error404Message.jsx
+++ frontend/VhyperGamesClient/src/components/errorComponent/Error404Message.jsx
@@ -1,17 +1,17 @@
-import classes from "./Error404Message.module.css";
+import styles from "./Error404Message.module.css";
 
 function Error404Message() {
   return (
-    <div className={classes.errorBody}>
-      <h1>ERROR 404!</h1>
+    <div className={styles.errorBody}>
+      <h1 className={styles.errorTitle}>ERROR 404!</h1>
 
       <img
-        className={classes.gif}
+        className={styles.gif}
         src="/img/goku.gif"
         alt="gif de kirby andando"
       />
 
-      <div>
+      <div className={styles.errorText}>
         <h2>Nuestro equipo se dirige a resolver el problema</h2>
         <h2>Por favor ten paciencia</h2>
       </div>
