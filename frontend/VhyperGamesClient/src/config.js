3248bfd8a1775014ca969cfdcc999a1a6f522890
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-04 10:56:10 +0100

    Mas endpoint

--- frontend/VhyperGamesClient/src/config.js
+++ frontend/VhyperGamesClient/src/config.js
@@ -2,3 +2,5 @@
 export const API_BASE_URL = 'https://localhost:7207/api/';
 export const LOGIN_ENDPOINT = `${API_BASE_URL}auth/login`;
 export const REGISTER_ENDPOINT = `${API_BASE_URL}auth/register`;
+export const CATALOG_SALES = `${API_BASE_URL}catalog/sales`;
+export const CATALOG_NEW_RELEASES = `${API_BASE_URL}catalog/new-releases`;
