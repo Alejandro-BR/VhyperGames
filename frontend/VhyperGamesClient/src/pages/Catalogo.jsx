60fc9459c48c06fe924de84e487be64cb98ef94c
Raquel <rlopber070@g.educaand.es>
2024-11-04 13:11:03 +0100

    Cambiado el catalogo

--- frontend/VhyperGamesClient/src/pages/Catalogo.jsx
+++ frontend/VhyperGamesClient/src/pages/Catalogo.jsx
@@ -1,12 +1,13 @@
 import Header from "../components/headerComponent/Header";
 import Footer from "../components/footerComponent/Footer";
 import CatalogFilters from "../components/catalogComponent/CatalogFilters";
+import Title from "../components/titleComponent/Title";
 
 function Catalogo() {
   return (
     <>
       <Header/>
-      <h1>CATÁLOGO</h1>
+      <Title text="CATÁLOGO" size="3em" color="#fff" align="center"/>
       <CatalogFilters/>
       <Footer/>
     </>
