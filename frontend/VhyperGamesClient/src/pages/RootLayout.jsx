9cf4930ac27b1fa4d6ce50aaa7c5cdbdca428d13
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2024-11-07 11:54:05 +0100

    React Router Dom hecho

--- frontend/VhyperGamesClient/src/pages/RootLayout.jsx
+++ frontend/VhyperGamesClient/src/pages/RootLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/headerComponent/Header";
 
 function RootLayout() {
   return (
     <>
+      <Header />
       <main>
         <Outlet />
       </main>
