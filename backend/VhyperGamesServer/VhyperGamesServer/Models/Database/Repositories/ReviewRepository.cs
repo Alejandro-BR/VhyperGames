f7380af0e5b89fb37a1644fba9d822ba9e99b8ba
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-10 12:55:31 +0100

    Review Repository

---
+++ backend/VhyperGamesServer/VhyperGamesServer/Models/Database/Repositories/ReviewRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VhyperGamesServer.Models.Database.Entities;
+
+namespace VhyperGamesServer.Models.Database.Repositories;
+
+public class ReviewRepository : Repository<Review, int>
+{
+    private readonly MyDbContext _context;
+
+    public ReviewRepository(MyDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Review>> GetAllReviewsOrderByDateByGameId(int idGame)
+    {
+        return await GetQueryable()
+            .Where(r => r.GameId == idGame)
+            .OrderByDescending(r => r.ReviewDate)
+            .ToListAsync();
+    }
+
+    public async Task<Review> IsReviewed(int idGame, int idUser)
+    {
+        if (idGame <= -1 || idUser <= -1)
+        {
+            return null;
+        }
+
+        return await GetQueryable()
+            .FirstOrDefaultAsync(r => r.UserId == idUser && r.GameId == idGame);
+    }
+
+}
