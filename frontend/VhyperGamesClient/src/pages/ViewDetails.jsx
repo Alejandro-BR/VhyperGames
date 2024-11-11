d888c52a4e12fe08598ed2db7c16368889b8253e
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-11 12:47:38 +0100

    Vista detalle estructura

--- frontend/VhyperGamesClient/src/pages/ViewDetails.jsx
+++ frontend/VhyperGamesClient/src/pages/ViewDetails.jsx
@@ -1,13 +1,26 @@
 import Footer from "../components/footerComponent/Footer";
+import classes from "../styles/ViewDetails.module.css";
 
 function ViewDetails() {
-  return <>
-    <div className="generalContainer">
-      <p>Vista detalle.</p>
-    </div>
-    <Footer></Footer>
-  </>
+  return (
+    <>
+      <div className={classes.container}>
+        <div className={`${classes["main-section"]} ${classes.box}`}>
 
+        </div>
+        <div className={`${classes["price-newreview"]} ${classes.box}`}>
+          
+        </div>
+        <div className={`${classes["system-requirements"]} ${classes.box}`}>
+
+        </div>
+        <div className={`${classes["reviews"]} ${classes.box}`}>
+
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default ViewDetails;
