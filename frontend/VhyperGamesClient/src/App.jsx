300dd70e9f03ea9dbdec0fc63c20288f4b447a77
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-11 12:07:45 +0100

    Vista detalle creada

--- frontend/VhyperGamesClient/src/App.jsx
+++ frontend/VhyperGamesClient/src/App.jsx
@@ -2,7 +2,8 @@ import RootLayout from './pages/RootLayout';
 import Home from './pages/Home';
 import Catalogo from './pages/Catalogo';
 import Error from './pages/Error';
-import SobreNosotros from './pages/SobreNosotros'
+import SobreNosotros from './pages/SobreNosotros';
+import ViewDetails from './pages/ViewDetails';
 
 import {
   createBrowserRouter,
@@ -17,8 +18,8 @@ const routeDefinition = createRoutesFromElements(
     <Route index element={<Home />} />
     <Route path="catalogo" element={<Catalogo />} />
     <Route path="error" element={<Error />} />
-    {/* <Route path="details" element={<Details />} /> */}
     <Route path="sobre-nosotros" element={<SobreNosotros />} />
+    <Route path="juego/:gameId" element={<ViewDetails />} />
   </Route>
 );
 
---
+++ frontend/VhyperGamesClient/src/pages/ViewDetails.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/footerComponent/Footer";
+
+function ViewDetails() {
+  return <>
+    <div className="generalContainer">
+      <p>Vista detalle.</p>
+    </div>
+    <Footer></Footer>
+  </>
+
+}
+
+export default ViewDetails;
