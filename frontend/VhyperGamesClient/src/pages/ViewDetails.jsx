9d37d5409c21a8cd84da9985fee84853e0e681f2
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-11 12:52:52 +0100

    Metodologia BEM

--- frontend/VhyperGamesClient/src/pages/ViewDetails.jsx
+++ frontend/VhyperGamesClient/src/pages/ViewDetails.jsx
@@ -4,17 +4,17 @@ import classes from "../styles/ViewDetails.module.css";
 function ViewDetails() {
   return (
     <>
-      <div className={classes.container}>
-        <div className={`${classes["main-section"]} ${classes.box}`}>
+      <div className={classes["view-details"]}>
+        <div className={`${classes["view-details__main-section"]} ${classes.box}`}>
 
         </div>
-        <div className={`${classes["price-newreview"]} ${classes.box}`}>
+        <div className={`${classes["view-details__price-newreview"]} ${classes.box}`}>
           
         </div>
-        <div className={`${classes["system-requirements"]} ${classes.box}`}>
+        <div className={`${classes["view-details__system-requirements"]} ${classes.box}`}>
 
         </div>
-        <div className={`${classes["reviews"]} ${classes.box}`}>
+        <div className={`${classes["view-details__reviews"]} ${classes.box}`}>
 
         </div>
       </div>
