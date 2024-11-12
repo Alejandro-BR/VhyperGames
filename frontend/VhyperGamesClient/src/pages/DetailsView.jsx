745076e5331cc850373d8a0550a1136c52d8105b
pablorzdmngz <pablorzdmngz@gmail.com>
2024-11-12 13:07:03 +0100

    Correcciones inglés

--- frontend/VhyperGamesClient/src/pages/DetailsView.jsx
+++ frontend/VhyperGamesClient/src/pages/DetailsView.jsx
@@ -1,23 +1,25 @@
 import Footer from "../components/footerComponent/Footer";
-import GameData from "../components/viewDetailsComponent/gameDataComponent/GameData";
-import classes from "../styles/ViewDetails.module.css";
+import GameData from "../components/detailsViewComponents/gameDataComponent/GameData";
+import classes from "../styles/DetailsView.module.css";
 
 function ViewDetails() {
   return (
     <>
-      <div className={classes["view-details"]}>
-        <div className={`${classes["view-details__main-section"]} ${classes.box}`}>
+      <div className={`${classes["view-details"]} generalContainer`}>
+        <div
+          className={`${classes["view-details__main-section"]} ${classes.box}`}
+        >
           <GameData />
         </div>
-        <div className={`${classes["view-details__price-newreview"]} ${classes.box}`}>
-          
-        </div>
-        <div className={`${classes["view-details__system-requirements"]} ${classes.box}`}>
-
-        </div>
-        <div className={`${classes["view-details__reviews"]} ${classes.box}`}>
-
-        </div>
+        <div
+          className={`${classes["view-details__price-newreview"]} ${classes.box}`}
+        ></div>
+        <div
+          className={`${classes["view-details__system-requirements"]} ${classes.box}`}
+        ></div>
+        <div
+          className={`${classes["view-details__reviews"]} ${classes.box}`}
+        ></div>
       </div>
       <Footer />
     </>
