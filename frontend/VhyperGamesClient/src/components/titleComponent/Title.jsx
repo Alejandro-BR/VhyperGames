5b76dcc17190ca645d28b9ff64aef08ec182b4a5
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-12 11:50:08 +0100

    Mejora en el titulo y mostrar datos basicos de los juegos

--- frontend/VhyperGamesClient/src/components/titleComponent/Title.jsx
+++ frontend/VhyperGamesClient/src/components/titleComponent/Title.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import classes from './Title.module.css';
 
-function Title({ text, size = '2em', color = '#ffffff', align = 'center' }) {
+function Title({ text, size = '2em', color = '#ffffff', align = 'center', variant }) {
   return (
-    <div className={classes['title-container']}>
-        <h1 style={{ fontSize: size, color: color, textAlign: align }}>
+    <div className={`${classes['title-container']} ${variant ? classes[variant] : ''}`}>
+      <h1 style={{ fontSize: size, color: color, textAlign: align }}>
         {text}
-        </h1>
+      </h1>
     </div>
   );
 }
--- frontend/VhyperGamesClient/src/components/viewDetailsComponent/gameDataComponent/GameData.jsx
+++ frontend/VhyperGamesClient/src/components/viewDetailsComponent/gameDataComponent/GameData.jsx
@@ -1,24 +1,38 @@
-import {DETAILS_VIEW_GAME_DATA} from "../../../config"
+import { useState, useEffect } from "react";
+import { DETAILS_VIEW_GAME_DATA } from "../../../config";
 import Title from "../../titleComponent/Title";
 
 function GameData({ id }) {
+  // Id estático para propósitos de este ejemplo
+  id = 2;
 
-  id = 1;
+  // Estado inicial para almacenar el objeto de juego
+  const [juego, setJuego] = useState(null);
 
-  const [juegos, setJuegos] = useState([]);
-
-  const fetchJuegos = async (id) => {
+  // Función para obtener los datos del juego
+  const fetchJuego = async (id) => {
     try {
       const response = await fetch(`${DETAILS_VIEW_GAME_DATA}?id=${id}`);
+      if (!response.ok) throw new Error("Error en la respuesta de la API");
+      const data = await response.json();
+      setJuego(data);  // Almacenar el objeto recibido en el estado
     } catch (error) {
-      
+      console.error("Error al obtener los datos:", error);
     }
-  }
+  };
+
+  // Llamar a fetchJuego cuando el componente se monte
+  useEffect(() => {
+    fetchJuego(id);
+  }, [id]);
 
   return (
     <>
-      <Title text={"Titulo Juego"} size="3em" color="#fff" align="right" />
 
+      <Title text={juego ? juego.title : "Cargando..."} size="3em" color="#fff" align="left" variant="tituloJuego" />
+      {juego && (
+        <p style={{ color: "#fff", textAlign: "left" }}>{juego.description}</p>
+      )}
     </>
   );
 }
