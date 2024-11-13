454871d23a2168bf69579b4adc41bf23f2465690
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-13 19:08:30 +0100

    Entidades de Carrito

---
+++ backend/VhyperGamesServer/VhyperGamesServer/Models/Database/Entities/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace VhyperGamesServer.Models.Database.Entities;
+
+public class Cart
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    [JsonIgnore]
+    public List<CartDetail> CartDetails { get; set; } = new List<CartDetail>();
+
+}
