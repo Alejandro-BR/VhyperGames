366ba060e3727ff8fa2f4e2545015c55c8de93f4
Raquel <rlopber070@g.educaand.es>
2024-11-14 14:10:18 +0100

    Autentificación

--- frontend/VhyperGamesClient/src/pages/DetailsView.jsx
+++ frontend/VhyperGamesClient/src/pages/DetailsView.jsx
@@ -3,12 +3,15 @@ import GameData from "../components/detailsViewComponents/gameDataComponent/Game
 import GamePrice from "../components/detailsViewComponents/gamePriceComponent/GamePrice"
 import classes from "../styles/DetailsView.module.css";
 import { useParams } from "react-router-dom";
+import ReviewEntry from "../components/detailsViewComponents/reviewEntryComponents/ReviewEntry";
 import GameRequeriments from "../components/detailsViewComponents/gameRequirements/GameRequirements";
+import { getVarSessionStorage } from "../utils/keep";
 
 function ViewDetails() {
 
   const params = useParams();
-
+  const isAuthenticated = !!getVarSessionStorage('accessToken');
+  console.log('isAuthenticated:', isAuthenticated);
   return (
     <>
       <div className={`${classes["view-details"]} generalContainer`}>
@@ -19,6 +22,10 @@ function ViewDetails() {
         </div>
         <div className={`${classes["view-details__price-newreview"]} ${classes.box}`}>
           <GamePrice id={params.gameId}/>
+          <ReviewEntry 
+            gameId={params.gameId} 
+            isAuthenticated={isAuthenticated} 
+          />
         </div>
         <div className={`${classes["view-details__system-requirements"]} ${classes.box}`}>
           <GameRequeriments id={params.gameId}/>
