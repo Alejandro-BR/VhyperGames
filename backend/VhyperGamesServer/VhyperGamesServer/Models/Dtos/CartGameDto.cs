c2ea6fa7de9fa126b2306f6027a225ec1f25b7ee
Jmolmel <josemolinamelendez@gmail.com>
2024-11-15 12:06:22 +0100

    Cart Dto

---
+++ backend/VhyperGamesServer/VhyperGamesServer/Models/Dtos/CartGameDto.cs
@@ -0,0 +1,20 @@
+﻿using VhyperGamesServer.Models.Database.Entities;
+
+namespace VhyperGamesServer.Models.Dtos;
+
+public class CartGameDto
+{
+    public int Id { get; set; }
+
+    public int IdGame { get; set; }
+
+    public string Title { get; set; }
+
+    public int Price { get; set; }
+
+    public ImageGame ImageGames { get; set; }
+
+    public int Quantity { get; set; }
+
+    public int Stock { get; set; }
+}
---
+++ backend/VhyperGamesServer/VhyperGamesServer/Models/Dtos/CartPaymentDto.cs
@@ -0,0 +1,13 @@
+﻿namespace VhyperGamesServer.Models.Dtos;
+
+public class CartPaymentDto
+{
+    public int UserId { get; set; }
+
+    public int CartId { get; set; }
+
+    public List<CartGameDto> Games { get; set; }
+
+    public int TotalPrice { get; set; }
+
+}
