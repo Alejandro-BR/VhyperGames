15de475fcdfe9c412d3e44665e02eeed0ee2dc6f
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-17 20:59:25 +0100

    Mejora en Button y CartPayment a falta del contexto

--- frontend/VhyperGamesClient/src/components/cartViewComponent/CartPayment.jsx
+++ frontend/VhyperGamesClient/src/components/cartViewComponent/CartPayment.jsx
@@ -1,16 +1,15 @@
 import { messageEuros , messageEthereum} from '../../helpers/messages';
 import { ConvertToDecimal } from "../../utils/price";
 import Button from "../buttonComponent/Button"
-
 import classes from "./CartPayment.module.css"
 
 function CartPayment() {
-  let precioBack = 4498;
+  let precioBack = 4498; // Hay que cogerlo desde el contexto 
   const precio = ConvertToDecimal(precioBack);
 
   return (<div className={classes.cartPayment} >
     <div className={classes.cartPayment__containerTitle}>
-      <p className={classes.cartPayment__palito}>❙</p>
+      <p className={classes.cartPayment__palito}>&#10073;</p>
       <p className={classes.cartPayment__title}>Carrito</p>
     </div>
     <p className={classes.cartPayment__total}>TOTAL</p>
@@ -23,7 +22,7 @@ function CartPayment() {
         variant={"medium"}
         color={"morado-azul"}
         onClick={messageEuros}
-      >PAGAR EN EUROS</Button>
+      >PAGAR EN <br/> EUROS</Button>
       <Button
         variant={"medium"}
         color={"azul-morado"}
