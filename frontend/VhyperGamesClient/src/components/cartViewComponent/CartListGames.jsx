9327c7c45205c829c1f5bf2e33883fa8c3b4ea06
Raquel <rlopber070@g.educaand.es>
2024-11-17 19:01:33 +0100

    estructura cartlistgames

---
+++ frontend/VhyperGamesClient/src/components/cartViewComponent/CartListGames.jsx
@@ -0,0 +1,16 @@
+import classes from "./CartListGames.module.css";
+
+const CartListGames = () => {
+  return (
+    <div className={classes.container}>
+      <img></img>
+      <div className={classes.containertext}>
+        <h1></h1>
+        <p></p>
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default CartListGames;
