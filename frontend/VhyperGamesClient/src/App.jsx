d40b0e8c9a9fe2c5b62a480b8896b6d47f3f42c4
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-19 11:47:27 +0100

    Cambios de nombres

--- frontend/VhyperGamesClient/src/App.jsx
+++ frontend/VhyperGamesClient/src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home';
 import Catalogo from './pages/Catalogo';
 import Error from './pages/Error';
 import SobreNosotros from './pages/SobreNosotros';
-import DetailsViewLegacy from './pages/DetailsViewLegacy';
+import DetailsView from './pages/DetailsView';
 import { AuthProvider } from '../src/context/authcontext';
 import { CartProvider } from './context/CartContext';
 import Cart from './pages/Cart';
@@ -15,14 +15,13 @@ import {
   Route,
 } from "react-router-dom";
 
-
 const routeDefinition = createRoutesFromElements(
   <Route path="/" element={<RootLayout />} errorElement={<Error />}>
     <Route index element={<Home />} />
     <Route path="catalogo" element={<Catalogo />} />
     <Route path="error" element={<Error />} />
     <Route path="sobre-nosotros" element={<SobreNosotros />} />
-    <Route path="juego/:gameId" element={<DetailsViewLegacy />} />
+    <Route path="juego/:gameId" element={<DetailsView />} />
     <Route path="cart" element={<Cart />} />
   </Route>
 );
