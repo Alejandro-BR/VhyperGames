de35420eb06ca31d9aa679182717954d66d21f78
fernandosanchez1609 <fsanlag686@g.educaand.es>
2024-11-20 15:38:53 +0100

    css del carrito

--- frontend/VhyperGamesClient/src/pages/Cart.jsx
+++ frontend/VhyperGamesClient/src/pages/Cart.jsx
@@ -1,11 +1,26 @@
 import CartListGames from "../components/cartViewComponent/CartListGames";
 import CartPayment from "../components/cartViewComponent/CartPayment";
+import classes from "../styles/Cart.module.css"
 
 function Cart() {
     return (
-        <div>
+        <div className={classes.container}>
+            <div className={classes.cartListGames}>
             <CartListGames />
+            <CartListGames />
+            <CartListGames />
+            <CartListGames />
+            <CartListGames />
+            <CartListGames />
+            <CartListGames />
+            <CartListGames />
+            <CartListGames />
+            </div>
+            
+            <div className={classes.cartPayment}>
             <CartPayment />
+            </div>
+            
         </div>
     )
 }
