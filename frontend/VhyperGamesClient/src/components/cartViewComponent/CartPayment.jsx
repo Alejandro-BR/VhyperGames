874becd87fb7b10fe45f255ba68e229f8bfa3eb5
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-21 12:48:41 +0100

    CartPayment Funciona

--- frontend/VhyperGamesClient/src/components/cartViewComponent/CartPayment.jsx
+++ frontend/VhyperGamesClient/src/components/cartViewComponent/CartPayment.jsx
@@ -1,37 +1,52 @@
-import { messageEuros , messageEthereum} from '../../helpers/messages';
+import { useContext, useState, useEffect } from 'react';
+import { messageEuros, messageEthereum } from '../../helpers/messages';
 import { CreateData } from '../../utils/dataCart';
-import { ConvertToDecimal, TotalPrice } from "../../utils/price";
-import Button from "../buttonComponent/Button"
-import classes from "./CartPayment.module.css"
+import { ConvertToDecimal, TotalPrice } from '../../utils/price';
+// import { getVarLS } from '../../utils/keep';
+import { CartContext } from '../../context/CartContext';
+import Button from '../buttonComponent/Button';
+import classes from './CartPayment.module.css';
 
 function CartPayment() {
-  let data = CreateData();
-  data = TotalPrice(data); 
-  const precio = ConvertToDecimal(data);
+  const { gameDetails, items } = useContext(CartContext); 
+  const [data, setData] = useState([]); 
 
-  return (<div className={classes.cartPayment} >
-    <div className={classes.cartPayment__containerTitle}>
-      <p className={classes.cartPayment__palito}>&#10073;</p>
-      <p className={classes.cartPayment__title}>Carrito</p>
+  useEffect(() => {
+    const updatedData = CreateData(items, gameDetails);
+    setData(updatedData);
+  }, [gameDetails, items]); 
+
+  const precio = ConvertToDecimal(TotalPrice(data)); 
+
+  return (
+    <div className={classes.cartPayment}>
+      <div className={classes.cartPayment__containerTitle}>
+        <p className={classes.cartPayment__palito}>&#10073;</p>
+        <p className={classes.cartPayment__title}>Carrito</p>
+      </div>
+      <p className={classes.cartPayment__total}>TOTAL</p>
+      <div className={classes.cartPayment__price}>
+        <p>{precio} €</p>
+      </div>
+      <hr className={classes.cartPayment__line} />
+      <div className={classes.cartPayment__containerButtons}>
+        <Button
+          variant={"medium"}
+          color={"morado-azul"}
+          onClick={messageEuros}
+        >
+          PAGAR EN <br /> EUROS
+        </Button>
+        <Button
+          variant={"medium"}
+          color={"azul-morado"}
+          onClick={messageEthereum}
+        >
+          PAGAR EN ETHEREUM
+        </Button>
+      </div>
     </div>
-    <p className={classes.cartPayment__total}>TOTAL</p>
-    <div className={classes.cartPayment__price}>
-      <p >{precio} €</p>
-    </div>
-    <hr className={classes.cartPayment__line}/>
-    <div className={classes.cartPayment__containerButtons}>
-      <Button
-        variant={"medium"}
-        color={"morado-azul"}
-        onClick={messageEuros}
-      >PAGAR EN <br/> EUROS</Button>
-      <Button
-        variant={"medium"}
-        color={"azul-morado"}
-        onClick={messageEthereum}
-      >PAGAR EN ETHEREUM</Button>
-    </div>
-  </div>);
+  );
 }
 
 export default CartPayment;
--- frontend/VhyperGamesClient/src/utils/dataCart.js
+++ frontend/VhyperGamesClient/src/utils/dataCart.js
@@ -1,71 +1,18 @@
-export function CreateData() {
-  const localstorage = [
-    {
-      "gameId": 1,
-      "quantity": 6
-    },
-    {
-      "gameId": 2,
-      "quantity": 4
-    },
-    {
-      "gameId": 12,
-      "quantity": 1
-    }
-  ];
+export function CreateData(ls, gameDetails) {
 
-  const dataBack = [
-    {
-      "idGame": 1,
-      "title": "The Witcher III",
-      "price": 1799,
-      "imageGame": {
-        "id": 1,
-        "gameId": 1,
-        "imageUrl": "images/TheWitcher3/thewitcher3.png",
-        "altText": "Banner de The Witcher III"
-      },
-      "stock": 100
-    },
-    {
-      "idGame": 2,
-      "title": "The Elder Scroll V: Skyrim",
-      "price": 2999,
-      "imageGame": {
-        "id": 7,
-        "gameId": 2,
-        "imageUrl": "images/Skyrim/skyrim.png",
-        "altText": "Imagen 1 de Skyrim"
-      },
-      "stock": 5
-    },
-    {
-      "idGame": 12,
-      "title": "Five Nights at Freddy's 2",
-      "price": 799,
-      "imageGame": {
-        "id": 48,
-        "gameId": 12,
-        "imageUrl": "images/FiveNightsAtFreddys2/Freddy2.png",
-        "altText": "Imagen 1 de fnaf2"
-      },
-      "stock": 100
-    }
-  ];
-
-  let data = [];
-
-  localstorage.forEach(localItem => {
-    const backGame = dataBack.find(game => game.idGame === localItem.gameId);
+  const data = ls.map(localItem => {
+    const gameId = Number(localItem.gameId);
+    const backGame = gameDetails.find(game => game.idGame === gameId);
 
     if (backGame) {
-      data.push({
-        price: backGame.price,
-        quantity: localItem.quantity
-      });
+      return {
+        price: backGame.price,  
+        quantity: localItem.quantity, 
+      };
     }
-  });
 
-  return data;
+    return null; 
+  }).filter(item => item !== null);  
+
+  return data; 
 }
-
