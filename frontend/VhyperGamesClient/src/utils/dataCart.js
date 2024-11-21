95b556533c7dd0262d917198c0f40fac30ad7085
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-21 11:22:59 +0100

    Solucion de errores

---
+++ frontend/VhyperGamesClient/src/utils/dataCart.js
@@ -0,0 +1,32 @@
+// let data = [{price: 1200, quantity: 1},{price: 3099, quantity: 2}];
+
+export function CreateData() {
+  const localstorage = [
+    {
+      "gameId": 1,
+      "quantity": 14
+    },
+    {
+      "gameId": 1,
+      "quantity": 14
+    },
+    {
+      "gameId": 3,
+      "quantity": 3
+    },
+    {
+      "gameId": 1,
+      "quantity": 42
+    },
+    {
+      "gameId": 11,
+      "quantity": 12
+    },
+    {
+      "gameId": 14,
+      "quantity": 5
+    }
+  ];
+
+  
+}
