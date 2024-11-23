2e7ca988e5ebf1e54b6c034a96b05bca1e509bc7
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-23 12:57:00 +0100

    Cart terminado :)

--- frontend/VhyperGamesClient/src/pages/Cart.jsx
+++ frontend/VhyperGamesClient/src/pages/Cart.jsx
@@ -1,19 +1,25 @@
 import CartListGames from "../components/cartViewComponent/CartListGames";
 import CartPayment from "../components/cartViewComponent/CartPayment";
 import classes from "../styles/Cart.module.css";
+import Footer from "../components/footerComponent/Footer"
 
 function Cart() {
-    return (
+  return (
+    <div>
+      <div className="generalContainer">
         <div className={classes.containerCartList}>
-            <div className={classes.cartListGames}>
-                <CartListGames />
-            </div>
-
-            <div className={classes.cartPayment}>
-                <CartPayment />
-            </div>
+          <div className={classes.cartListGames}>
+            <CartListGames />
+          </div>
+          <div className={classes.cartPayment}>
+            <CartPayment />
+          </div>
         </div>
-    );
+      </div>
+      <Footer />
+    </div>
+
+  );
 }
 
 export default Cart;
