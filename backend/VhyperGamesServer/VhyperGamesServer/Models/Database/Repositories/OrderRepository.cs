b7c68e9ff583d0c13e919b26557507c7dc5ecc06
pablo-r-d <pruidomo319@g.educaand.es>
2024-11-23 01:28:17 +0100

    Repositorios de Reserve, ReserveDetails, Order y OrderDetails

---
+++ backend/VhyperGamesServer/VhyperGamesServer/Models/Database/Repositories/OrderRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VhyperGamesServer.Models.Database.Entities;
+
+namespace VhyperGamesServer.Models.Database.Repositories;
+
+public class OrderRepository : Repository<Order, int>
+{
+    private readonly MyDbContext _context;
+    public OrderRepository(MyDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<Reserve> GetOrderById(int id)
+    {
+        return await Context.Set<Reserve>()
+            .Include(c => c.ReserveDetails)
+                .ThenInclude(cd => cd.Game)
+                    .ThenInclude(g => g.ImageGames)
+            .FirstOrDefaultAsync(c => c.Id == id);
+    }
+}
---
+++ backend/VhyperGamesServer/VhyperGamesServer/Models/Database/Repositories/ReserveRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VhyperGamesServer.Models.Database.Entities;
+
+namespace VhyperGamesServer.Models.Database.Repositories;
+
+public class ReserveRepository : Repository<Reserve, int>
+{
+    private readonly MyDbContext _context;
+    public ReserveRepository(MyDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<Reserve> GetReserveById(int id)
+    {
+        return await Context.Set<Reserve>()
+            .Include(c => c.ReserveDetails)
+                .ThenInclude(cd => cd.Game)
+                    .ThenInclude(g => g.ImageGames)
+            .FirstOrDefaultAsync(c => c.Id == id);
+    }
+}
