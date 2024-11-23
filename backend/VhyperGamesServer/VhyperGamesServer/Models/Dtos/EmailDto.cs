7dd6a294d4f4dfe69ca0d5fb27a04e89db543e62
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-23 19:24:06 +0100

    EMAIL

---
+++ backend/VhyperGamesServer/VhyperGamesServer/Models/Dtos/EmailDto.cs
@@ -0,0 +1,8 @@
+﻿namespace VhyperGamesServer.Models.Dtos;
+
+public class EmailDto
+{
+    public string Para { get; set; } = string.Empty;
+    public string Asunto {  get; set; } = string.Empty;
+    public string Contenido {  get; set; } = string.Empty;  
+}
--- backend/VhyperGamesServer/VhyperGamesServer/Utilities/EmailHelper.cs
+++ backend/VhyperGamesServer/VhyperGamesServer/Utilities/EmailHelper.cs
@@ -9,15 +9,22 @@ internal class EmailHelper
     private const int SMTP_PORT = 587;
     private const string EMAIL_FROM = "vhypergames24@gmail.com";
     // Se obtiene de este video https://www.youtube.com/watch?v=Yv_Wh0zjMw4
-    private const string PASSWORD_EMAIL_FROM = "vhyperGames_2024!";
+    private const string PASSWORD_EMAIL_FROM = "EMAIL_KEY";
 
     public static async Task SendEmailAsync(string to, string subject, string body, bool isHtml = false)
     {
+        string key = Environment.GetEnvironmentVariable(PASSWORD_EMAIL_FROM);
+
+        if (string.IsNullOrEmpty(key))
+        {
+            throw new InvalidOperationException("EMAIL_KEY is not configured in environment variables.");
+        }
+
         using SmtpClient client = new SmtpClient(SMTP_HOST, SMTP_PORT)
         {
             EnableSsl = true,
             UseDefaultCredentials = false,
-            Credentials = new NetworkCredential(EMAIL_FROM, PASSWORD_EMAIL_FROM)
+            Credentials = new NetworkCredential(EMAIL_FROM, key)
         };
 
         MailMessage mail = new MailMessage(EMAIL_FROM, to, subject, body)
