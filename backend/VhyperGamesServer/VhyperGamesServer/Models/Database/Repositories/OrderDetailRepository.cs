f1a7443c9eb4eae40f978fae538ff2ebab720d5e
Raquel <rlopber070@g.educaand.es>
2024-11-24 20:33:33 +0100

    Creado OrderDetailsRepository

---
+++ backend/VhyperGamesServer/VhyperGamesServer/Models/Database/Repositories/OrderDetailRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VhyperGamesServer.Models.Database.Entities;
+
+namespace VhyperGamesServer.Models.Database.Repositories;
+
+public class OrderDetailRepository : Repository<OrderDetail, int>
+{
+    private readonly MyDbContext _context;
+
+    public OrderDetailRepository(MyDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    // Para la vista de usuario
+    public async Task<List<OrderDetail>> GetOrderById(int idOrder)
+    {
+        return await Context.Set<OrderDetail>()
+        .Where(cd => cd.OrderId == idOrder)
+        .Include(cd => cd.Game)
+            .ThenInclude(g => g.ImageGames)
+        .ToListAsync();
+    }
+}
