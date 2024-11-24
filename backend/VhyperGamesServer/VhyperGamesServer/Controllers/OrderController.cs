5e3b60e0043379b62bc61bbfbadca7fb61364917
Raquel <rlopber070@g.educaand.es>
2024-11-24 20:32:40 +0100

    Creado Order Controller

---
+++ backend/VhyperGamesServer/VhyperGamesServer/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VhyperGamesServer.Models.Dtos;
+using VhyperGamesServer.Services;
+
+namespace VhyperGamesServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class OrderController : ControllerBase
+    {
+        private readonly OrderService _orderService;
+
+        public OrderController(OrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet("user/{userId}/recent")]
+        public async Task<IActionResult> GetRecentOrderByUserId(int userId)
+        {
+            try
+            {
+                var order = await _orderService.GetRecentOrderByUserIdAsync(userId);
+                return Ok(order);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Error inesperado.", detail = ex.Message });
+            }
+        }
+    }
+}
+
