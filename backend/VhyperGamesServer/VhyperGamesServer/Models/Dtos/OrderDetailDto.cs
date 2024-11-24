860c929bd5455acce1ec68e03c79170b94b1fad0
Raquel <rlopber070@g.educaand.es>
2024-11-24 20:34:28 +0100

    cambio de nombre

---
+++ backend/VhyperGamesServer/VhyperGamesServer/Models/Dtos/OrderDetailDto.cs
@@ -0,0 +1,13 @@
+﻿using VhyperGamesServer.Models.Database.Entities;
+
+namespace VhyperGamesServer.Models.Dtos;
+
+public class OrderDetailDto
+{
+    public int GameId { get; set; }
+    public string Title { get; set; }
+    public int Quantity { get; set; }
+    public int Price { get; set; }
+    public ImageGame ImageGame { get; set; }
+
+}
