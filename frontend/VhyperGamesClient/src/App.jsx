9f6973a353f058818ecb5e5be873a9793b286f53
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-24 12:22:26 +0100

    Checkout

--- frontend/VhyperGamesClient/src/App.jsx
+++ frontend/VhyperGamesClient/src/App.jsx
@@ -3,10 +3,11 @@ import Home from './pages/Home';
 import Catalogo from './pages/Catalogo';
 import Error from './pages/Error';
 import SobreNosotros from './pages/SobreNosotros';
+import Checkout from './pages/Checkout';
+import Cart from './pages/Cart';
 import DetailsView from './pages/DetailsView';
 import { AuthProvider } from '../src/context/authcontext';
 import { CartProvider } from './context/CartContext';
-import Cart from './pages/Cart';
 
 import {
   createBrowserRouter,
@@ -23,6 +24,7 @@ const routeDefinition = createRoutesFromElements(
     <Route path="sobre-nosotros" element={<SobreNosotros />} />
     <Route path="juego/:gameId" element={<DetailsView />} />
     <Route path="cart" element={<Cart />} />
+    <Route path="checkout/:modo" element={<Checkout />}/>
   </Route>
 );
 
