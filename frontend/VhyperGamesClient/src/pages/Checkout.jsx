4a10b55951779648da76f6231501ce25b3aaf525
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-26 17:45:51 +0100

    CSS

--- frontend/VhyperGamesClient/src/pages/Checkout.jsx
+++ frontend/VhyperGamesClient/src/pages/Checkout.jsx
@@ -1,17 +1,18 @@
 import Footer from "../components/footerComponent/Footer";
 import { useParams } from "react-router-dom";
 import CheckoutForm from "../components/stripe/CheckoutForm";
+import classes from "../styles/checkout.module.css"
 
 function Checkout() {
   const params = useParams();
 
   return (
-    <div>
-      <div>
-        <div>
+    <div className={classes["checkout"]}>
+      <div className={classes["checkout__components"]}>
+        <div className={classes["checkout__list"]}>
           Lista de juegos
         </div>
-        <div>
+        <div className={classes["checkout__pay"]}>
           {params.modo === "euros" ? <CheckoutForm /> : <p>hola</p>}
         </div>
       </div>
