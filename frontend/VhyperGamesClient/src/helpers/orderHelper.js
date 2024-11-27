2894837421ca88927017ca09962894a4cc86e204
Jmolmel <josemolinamelendez@gmail.com>
2024-11-27 18:13:47 +0100

    PaymentOrder terminado

--- frontend/VhyperGamesClient/src/helpers/orderHelper.js
+++ frontend/VhyperGamesClient/src/helpers/orderHelper.js
@@ -1,19 +1,16 @@
-export const createReserve = async (url, token) => {
-  const response = await fetch(url, {
-    method: 'GET',
+export const recentOrder = async (url, token) => {
+  const response = await fetch(`${url}`, {
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
   });
 
   if (!response.ok) {
-    const error = await response.json();
-    throw new Error(error.message || 'Error al obtener la reserva.');
+    const errorText = await response.text(); 
+    throw new Error(`Error al obtener la reserva: ${response.status} - ${errorText}`);
   }
 
-  // Almacenar todos los datos en un solo objeto
-  const reserveData = await response.json();
-
-  return reserveData;
+  return await response.json();
 };
