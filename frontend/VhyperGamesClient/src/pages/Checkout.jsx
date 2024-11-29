6b89dcf8ba2d33e17e0583969447d2d50ec28bf1
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-29 11:35:47 +0100

    Checkout timer

--- frontend/VhyperGamesClient/src/pages/Checkout.jsx
+++ frontend/VhyperGamesClient/src/pages/Checkout.jsx
@@ -1,11 +1,21 @@
 import Footer from "../components/footerComponent/Footer";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import CheckoutForm from "../components/stripe/CheckoutForm";
 import classes from "../styles/checkout.module.css"
 import CheckoutList from "../components/CheckoutComponent/CheckoutList";
+import { useEffect } from "react";
 
 function Checkout() {
   const params = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/cart")
+    }, 180000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
 
   return (
     <div className={classes["checkout"]}>
