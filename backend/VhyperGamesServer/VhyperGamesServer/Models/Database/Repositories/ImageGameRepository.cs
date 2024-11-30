e50c9f0a7e19759cff26bf6d17d7ad236b4677d4
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-30 19:35:14 +0100

    Repository de imagen

--- backend/VhyperGamesServer/VhyperGamesServer/Models/Database/Repositories/ImageGameRepository.cs
+++ backend/VhyperGamesServer/VhyperGamesServer/Models/Database/Repositories/ImageGameRepository.cs
@@ -1,10 +1,19 @@
-﻿using VhyperGamesServer.Models.Database.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using VhyperGamesServer.Models.Database.Entities;
 
 namespace VhyperGamesServer.Models.Database.Repositories;
 
 public class ImageGameRepository : Repository<ImageGame, int>
 {
+    private readonly MyDbContext _context;
+
     public ImageGameRepository(MyDbContext context) : base(context)
     {
+        _context = context;
+    }
+
+    public async Task<List<ImageGame>> GetAllImagesGamesAsync()
+    {
+        return await _context.ImagesGame.Include(ig => ig.Game).ToListAsync(); 
     }
 }
