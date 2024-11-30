c943bb621e1f371a1e423f1f1b60f7401aad19d2
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-11-30 19:35:27 +0100

    Mappers

--- backend/VhyperGamesServer/VhyperGamesServer/Models/Mappers/AdminMapper.cs
+++ backend/VhyperGamesServer/VhyperGamesServer/Models/Mappers/AdminMapper.cs
@@ -1,4 +1,6 @@
-﻿using VhyperGamesServer.Models.Database.Entities;
+﻿using Microsoft.AspNetCore.StaticFiles;
+using System.Collections.Generic;
+using VhyperGamesServer.Models.Database.Entities;
 using VhyperGamesServer.Models.Dtos;
 
 namespace VhyperGamesServer.Models.Mappers;
@@ -16,17 +18,20 @@ public class AdminMapper
         };
     }
 
-    public List<AdminUserDto> ToListAdminUser(List<User> users) {
+    public List<AdminUserDto> ToListAdminUser(List<User> users)
+    {
         List<AdminUserDto> adminUserDtos = new List<AdminUserDto>();
 
-        foreach (User user in users) { 
+        foreach (User user in users)
+        {
             adminUserDtos.Add(ToAdminUserDto(user));
         }
 
         return adminUserDtos;
     }
 
-    public AdminGameDto ToAdminGameDto(Game game) {
+    public AdminGameDto ToAdminGameDto(Game game)
+    {
         return new AdminGameDto
         {
             Id = game.Id,
@@ -37,7 +42,8 @@ public class AdminMapper
         };
     }
 
-    public List<AdminGameDto> ToListAdminGameDto(List<Game> games) { 
+    public List<AdminGameDto> ToListAdminGameDto(List<Game> games)
+    {
         List<AdminGameDto> adminGameDtos = new List<AdminGameDto>();
 
         foreach (Game game in games)
@@ -48,11 +54,55 @@ public class AdminMapper
         return adminGameDtos;
     }
 
+    private string GetContentType(string path)
+    {
+        var provider = new FileExtensionContentTypeProvider();
+        if (!provider.TryGetContentType(path, out var contentType))
+        {
+            contentType = "application/octet-stream";
+        }
+        return contentType;
+    }
+
+
+    public ImageRequestDto ToImageRequestDto(ImageGame imageGame)
+    {
+        string filePath = imageGame.ImageUrl;
+
+        var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+        var formFile = new FormFile(fileStream, 0, fileStream.Length, "file", Path.GetFileName(filePath))
+        {
+            Headers = new HeaderDictionary(),
+            ContentType = GetContentType(filePath)
+        };
+
+        return new ImageRequestDto()
+        {
+            AltText = imageGame.AltText,
+            File = formFile
+        };
+    }
+
+    public List<ImageRequestDto> ToListImageRequestDto(List<ImageGame> imageGames)
+    {
+        List<ImageRequestDto> imageRequestDtos = new List<ImageRequestDto>();
+
+        foreach (ImageGame imageGame in imageGames) {
+            imageRequestDtos.Add(ToImageRequestDto(imageGame));
+        }
+
+        return imageRequestDtos;
+    }
+
+
     public AdminFormGameDto ToAdminFormGameDto(Game game)
     {
+        List<ImageRequestDto> imageRequestDtos = ToListImageRequestDto(game.ImageGames);
+
         return new AdminFormGameDto
         {
-            Id =game.Id,
+            Id = game.Id,
             Title = game.Title,
             Price = game.Price,
             Stock = game.Stock,
@@ -62,7 +112,7 @@ public class AdminMapper
             Genre = game.Genre,
             DrmFree = game.DrmFree,
             ReleaseDate = game.ReleaseDate,
-            ImageGames = game.ImageGames,
+            //ImageRequests = imageRequestDtos,
         };
     }
 }
