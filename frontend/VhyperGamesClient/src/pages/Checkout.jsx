206435dc854976ea1cf8f1c4f60f5a6e76e35cc0
Jmolmel <josemolinamelendez@gmail.com>
2024-12-01 21:59:11 +0100

    Ethereum v2

--- frontend/VhyperGamesClient/src/pages/Checkout.jsx
+++ frontend/VhyperGamesClient/src/pages/Checkout.jsx
@@ -4,6 +4,7 @@ import CheckoutForm from "../components/stripe/CheckoutForm";
 import classes from "../styles/checkout.module.css"
 import CheckoutList from "../components/CheckoutComponent/CheckoutList";
 import Timer from "../components/timers/Timer";
+import Ethereum from "../components/Ethereum/Ethereum";
 
 function Checkout() {
   const params = useParams();
@@ -18,7 +19,7 @@ function Checkout() {
           <CheckoutList/>
         </div>
         <div className={classes["checkout__pay"]}>
-          {params.modo === "euros" ? <CheckoutForm /> : <p>hola</p>}
+          {params.modo === "euros" ? <CheckoutForm /> : <Ethereum/>}
         </div>
       </div>
       <Footer />
