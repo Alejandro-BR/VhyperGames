9056df690a164c94b906382c05b78a1ef770af60
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-12-03 20:52:08 +0100

    Casi listo

--- frontend/VhyperGamesClient/src/components/usersManagement/userCardBlock.jsx
+++ frontend/VhyperGamesClient/src/components/usersManagement/userCardBlock.jsx
@@ -3,14 +3,18 @@ import UserCard from "./UserCard";
 import classes from "./UserCardBlock.module.css";
 import { AdminContext } from "../../context/AdminContext"
 import { useEffect } from "react";
+import { useAuth } from "../../context/authcontext";
 
 function UserCardBlock() {
 
   const { users, fetchUsers } = useContext(AdminContext);
+  const { token, decodedToken } = useAuth();
 
   useEffect(() => {
-    fetchUsers();
-  }, [users]);
+    if (token && decodedToken?.role === "Admin") {
+      fetchUsers();
+    };
+  }, [users, token, decodedToken?.role]);
 
   return (
     <section className={classes.productsList}>
