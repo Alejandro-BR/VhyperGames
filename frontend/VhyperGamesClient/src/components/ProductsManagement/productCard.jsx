1a57a414b2f25f32a7e2d516e39547c5258a0188
pablo-r-d <pruidomo319@g.educaand.es>
2024-12-04 00:26:50 +0100

    commit pre merge

--- frontend/VhyperGamesClient/src/components/ProductsManagement/productCard.jsx
+++ frontend/VhyperGamesClient/src/components/ProductsManagement/productCard.jsx
@@ -4,6 +4,8 @@ import { ConvertToDecimal } from "../../utils/price";
 import { useContext, useEffect, useState } from "react";
 import { AdminContext } from "../../context/AdminContext";
 import { useAuth } from "../../context/AuthContext";
+import GameFormModal from "../GameFormModal/GameFormModal";
+
 
 
 function ProductCard() {
@@ -11,18 +13,19 @@ function ProductCard() {
   const { games, fetchGames } = useContext(AdminContext);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [gameFormModal, setGameFormModal] = useState(false);
 
-    // Verificar si el usuario tiene el rol de admin antes de cargar los juegos
-    useEffect(() => {
-        console.log("Token en AdminProvider:", token);
-        console.log("decodedToken en AdminProvider:", decodedToken);
-      
-        if (token && decodedToken?.role === "Admin") {
-          console.log("Llamando a fetchGames...");
-          fetchGames();
-        }
-      }, [token, decodedToken]);
-      
+  // Verificar si el usuario tiene el rol de admin antes de cargar los juegos
+  useEffect(() => {
+    console.log("Token en AdminProvider:", token);
+    console.log("decodedToken en AdminProvider:", decodedToken);
+
+    if (token && decodedToken?.role === "Admin") {
+      console.log("Llamando a fetchGames...");
+      fetchGames();
+    }
+  }, [token, decodedToken]);
+
 
   // NO ENTIENDO POR QUÉ NO FUNCIONA EL FETCH NI EL MAP
 
@@ -38,30 +41,42 @@ function ProductCard() {
   }
 
   return (
-    <article className={classes.card}>
-      <Link to={`/juego/${id}`}>
-        <img src={imgUrl} alt={title} className={classes.gameImg} />
-      </Link>
+    <>
+      <article className={classes.card}>
+        <Link to={`/juego/${id}`}>
+          <img src={imgUrl} alt={title} className={classes.gameImg} />
+        </Link>
 
-      <div className={classes.container}>
-        <h2 className={classes.containerTitle}>Título</h2>
-        <p className={classes.title}>{title}</p>
-      </div>
+        <div className={classes.container}>
+          <h2 className={classes.containerTitle}>Título</h2>
+          <p className={classes.title}>{title}</p>
+        </div>
 
-      <div className={classes.container}>
-        <h2 className={classes.containerTitle}>Precio</h2>
-        <p className={classes.price}>{precio()} €</p>
-      </div>
+        <div className={classes.container}>
+          <h2 className={classes.containerTitle}>Precio</h2>
+          <p className={classes.price}>{precio()} €</p>
+        </div>
 
-      <div className={classes.container}>
-        <h2 className={classes.containerTitle}>Stock</h2>
-        <p className={classes.stock}>{stock}</p>
-      </div>
+        <div className={classes.container}>
+          <h2 className={classes.containerTitle}>Stock</h2>
+          <p className={classes.stock}>{stock}</p>
+        </div>
 
-      <button className={classes.editGame}>
-        <img src="/icon/edit-icon.svg" alt="editar producto" />
-      </button>
-    </article>
+        <button className={classes.editGame} onClick={() => setGameFormModal(true)}>
+          <img src="/icon/edit-icon.svg" alt="editar producto" />
+        </button>
+
+      </article>
+
+      {
+        gameFormModal && (
+          <GameFormModal
+            onClose={() => setGameFormModal(false)}
+          />
+        )
+      }
+    </>
+
   );
 }
 
