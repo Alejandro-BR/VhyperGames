b100493a53e06be2eb6159bd04f6602c9697cec4
Jmolmel <josemolinamelendez@gmail.com>
2024-12-03 22:43:39 +0100

    Cambio de rutas

--- frontend/VhyperGamesClient/src/components/Ethereum/Ethereum.jsx
+++ frontend/VhyperGamesClient/src/components/Ethereum/Ethereum.jsx
@@ -13,7 +13,7 @@ import {
 export const WALLET_METAMASK = import.meta.env.VITE_WALLET_METAMASK;
 import { useAuth } from "../../context/AuthContext";
 import { CheckoutContext } from "../../context/CheckoutContext";
-import Button from "../buttonComponent/Button";
+import Button from "../Buttons/Button";
 import { fetchReserveTotal } from "../../helpers/ethereumHelper";
 
 
