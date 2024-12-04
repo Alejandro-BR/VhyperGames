252f29feaf470bae1408b1fd3463aab29a714e08
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-12-04 08:45:27 +0100

    Cambios

--- frontend/VhyperGamesClient/src/components/ProductsManagement/SearchBar/SearchBarProduct.jsx
+++ frontend/VhyperGamesClient/src/components/ProductsManagement/SearchBar/SearchBarProduct.jsx
@@ -1,11 +1,21 @@
 import classes from "./SearchBarProduct.module.css"
+import { useContext } from "react";
+import { AdminContext } from "../../../context/AdminContext";
+import { useState, useEffect } from "react";
 
 function SearchBarProduct() {
-    return (
-        <div>
-            <input className={classes.searchBar} type="text" id="product-search" name="product-search" placeholder="Busca un producto" />
-        </div>
-    )
+
+  const { games, fetchGames } = useContext(AdminContext);
+
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+    <div>
+      <input className={classes.searchBar} type="text" id="product-search" name="product-search" placeholder="Busca un producto" />
+    </div>
+  )
 }
 
 export default SearchBarProduct
