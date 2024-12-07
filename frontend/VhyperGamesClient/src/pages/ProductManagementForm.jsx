72ef3778d1a2f6371d314dc499d2ce1ef0f06527
pablo-r-d <pruidomo319@g.educaand.es>
2024-12-07 03:12:31 +0100

    boton añadir y ligeros cambios css

--- frontend/VhyperGamesClient/src/pages/ProductManagementForm.jsx
+++ frontend/VhyperGamesClient/src/pages/ProductManagementForm.jsx
@@ -1,13 +1,16 @@
 import ImagesManager from "../components/ProductsManagement/Images/ImagesManager"
 import Title from "../components/Titles/Title";
 import ProductForm from "../components/ProductsManagement/GameFormModal/ProductForm";
+import classes from "../styles/ProductManagementForm.module.css"
 
 function ProductManagementForm() {
     return (
         <div className="generalContainer">
             <Title text="?:" size="3em" color="#fff" align="center" />
-            <ProductForm />
-            <ImagesManager />
+            <div className={classes.mainContainer}>
+                <ProductForm />
+                <ImagesManager />
+            </div>
         </div>
     )
 }
