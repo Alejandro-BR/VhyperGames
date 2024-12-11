8216cd0c911433755efa2f0d1bb405485b826b2a
Fernando Sanchez <146203186+Fernandosanchez1609@users.noreply.github.com>
2024-12-11 12:09:13 +0100

    cambios

--- frontend/VhyperGamesClient/src/pages/ProductManagement.jsx
+++ frontend/VhyperGamesClient/src/pages/ProductManagement.jsx
@@ -1,13 +1,13 @@
 
 import Title from "../components/Titles/Title";
-import ContainerManagementPorduct from "../components/ProductsManagement/ContainerManagementPorduct";
+import ContainerManagementProduct from "../components/ProductsManagement/ContainerManagementProduct";
 import ProductCardBlock from "../components/ProductsManagement/ProductCardBlock/ProductCardBlock";
 
 function ProductManagement() {
     return (
         <div className="generalContainer">
             <Title text="PRODUCTOS:" size="3em" color="#fff" align="center" />
-            <ContainerManagementPorduct />
+            <ContainerManagementProduct />
             <ProductCardBlock />
         </div>
     )
